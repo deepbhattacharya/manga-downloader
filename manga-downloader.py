544614fbbecd671a75c7b17a24c596abcb7b35f6
Deep Bhattacharya <the.secret.master.2007@gmail.com>
2014-09-16 19:15:05 -0700

    Added bato.to as a new URL type to be consistent

--- manga-downloader.py
+++ manga-downloader.py
@@ -269,7 +269,8 @@ class MangaStarkana(Manga):
             self.addMangaChapter(MangaChapterStarkana(self.name, ch_no, ch_url, ch_path))
 
 # Data structures that help instantiating the right subclasses based on URL
-URL_TYPES = {'http://www.batoto.net' : {'url' : '(http://)?(www\.)?(bato\.to|batoto\.net).+-r[0-9]+', 'manga' : MangaBatoto, 'mangachapter' : MangaChapterBatoto},
+URL_TYPES = {'http://www.batoto.net' : {'url' : '(http://)?(www\.)?(batoto\.net).+-r[0-9]+', 'manga' : MangaBatoto, 'mangachapter' : MangaChapterBatoto},
+            'http://www.bato.to' : {'url' : '(http://)?(www\.)?(bato\.to).+-r[0-9]+', 'manga' : MangaBatoto, 'mangachapter' : MangaChapterBatoto},
             'http://www.starkana.com' : {'url' : '(http://)?(www\.)?starkana\.com/manga/[0A-Z]/.+', 'manga' : MangaStarkana, 'mangachapter' : MangaChapterStarkana}
                 }
 
